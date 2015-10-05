aa2534d43a3f3c0c16671f8bee18c116f5eed362
cmrenus <cmrenus@gmail.com>
2015-10-05 16:04:53 -0400

    Revert "Started recipe and recipes layout"
    This reverts commit bf7fec11cae1b500764a28a24f3d2e0924ca3c29.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('recipals',['ngRoute', 'recipal.allRecipesCtrl', 'recipal.recipeCtrl']);
+var app = angular.module('recipals',['ngRoute', 'recipal.allRecipesCtrl']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider.
@@ -8,10 +8,6 @@ app.config(['$routeProvider', function($routeProvider){
     when('/recipes',{
     	templateUrl:"views/recipes.html",
         controller:"allRecipesCtrl"
-    }).
-    when('/recipes/:recipe_name',{
-        templateUrl:"views/recipeTemplate.html",
-        controller:"recipeCtrl"
     }).
     otherwise({
         redirectTo:'/'
