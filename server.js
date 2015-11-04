c6bb85cf57472a1db6fe87b0c09a5e8d05f61c67
Nicholas Fay <fayn@rpi.edu>
2015-11-03 23:55:05 -0500

    updated favicon

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.options("*", function (req, res) {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
