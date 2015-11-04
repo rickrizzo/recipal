b81d7e4cf8838f8623dcc532c1aa472da2097530
Robert Russo <rrusso44@gmail.com>
2015-11-03 23:38:55 -0500

    Finalize Form
    Added last elements

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use('/', express.static(path.join(__dirname, '/')));
 
 
 app.use('/', routes);
+//app.use('/newRecipe', );
 /*
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
